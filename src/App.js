e30183000255ff043b0acd841b5d6688df6821ab
Mohammad Rayhan <md.rayhan3451@gmail.com>
2021-05-03 18:14:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import data from './data';
 function App() {
+  const [count,setCount] =useState(0)
+  const [text,setText] =useState([])
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    let ammount =parseInt(count);
+    if (count <=0){
+      ammount=1;
+    }
+    if (count>8){
+      ammount=8;
+    }
+    setText(data.slice(0,ammount));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section-center">
+      <h3>lorem ipsum generator</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="ammount">paragraph :</label>
+        <input type="number" name="ammount" id="ammount" value={count} onChange={(e)=>setCount(e.target.value)}/>
+        <button className="btn" onClick={handleSubmit}>generate</button>
+      </form>
+      {text.map((item,index)=>{
+        return (
+          <p key={index}>{item}</p>
+        )
+      })}
+      
+    </section>
+    )
 }
 
 export default App;
